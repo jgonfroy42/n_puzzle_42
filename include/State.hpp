4d973c8e65bdf3291826f672f8bd18410619e0db
afaure <afaure@student.42.fr>
2023-10-09 15:42:59 +0200

    added comments to State class

--- include/State.hpp
+++ include/State.hpp
@@ -22,6 +22,27 @@ enum eval
 	conflicts,
 	hybrid,
 };
+
+
+/*
+*	IMPORTANT POINTS ABOUT STATE CLASS
+*
+*	-It uses static variables to store things such a size that are common
+*		to all instances so it is not possible to solve multiple puzzles at once
+*		
+*	- If you dont need to expand a node anymore you can delete its grid to optimize memory a bit
+*	
+*	- Every instances calculates its own hash that can be used in a hashmap by example
+*
+*	- it is possible to activate a transpoistion table to store every calculated score,
+*		usefull if you are using a searching algo that goes a lot through the same states ( IDA * i see you )
+*/
+
+/**
+ * @brief a Class representing a state of the npuzzle
+ * 
+ * 
+ */
 class State
 {
 	public:
@@ -37,46 +58,193 @@ class State
 		~State();
 		State(const State & other);
 		State(grid_format t_grid);
+		State & operator=(const State & other);
 		// State(const std::vector<int> & grid);
 		// State(grid_format t_grid, const State* t_parent);
 
+		/**	STATIC FUNCTIONS **/
+
+		/**
+		 * @brief get the size of the side of the current puzzle size
+		 * 
+		 * @return int
+		 */
 		static int getSideSize();
+
+		/**
+		 * @brief Get the area of the current puzzle size
+		 * 
+		 * @return int 
+		 */
 		static int getTotalSize();
+
+		/**
+		 * @brief Get the number of states ever constructed
+		 * 
+		 * @return int 
+		 */
 		static int getTotalStates();
+
+		/**
+		 * @brief Set the size (side size format) of the state class
+		 * this has effect on all instances.
+		 * 
+		 * @param n 
+		 */
 		static void setSize(const int & n);
-		// void setSize(const int & n);
-		
+
+		/**
+		 * @brief Set the evaluation function for every States to use
+		 * 
+		 * @param eval_set
+		 */
+		static void		set_eval(eval eval_set);
+
+		/**	OPERATORS **/
 		bool	operator==(grid_format cmp_grid) const;
+
+		/**
+		 * @brief lexicographical equality comparison of States' grids
+		 * 
+		 * @param other 
+		 * @return true if States have the same grid
+		 * @return false if States dont have the same grid
+		 */
 		bool operator==(const State & other) const ;
+
+		/**
+		 * @brief lexicographical inferiority comparison of States' grids
+		 * 
+		 * @param other 
+		 * @return true 
+		 * @return false 
+		 */
 		bool operator<(const State & other) const;
-		State & operator=(const State & other);
+
+
+		/** GETTERS & SETTERS **/
+
+		/**
+		 * @brief Get the underlying grid
+		 * 
+		 * @return a pointer to the grid
+		 */
 		optimized_grid	get_grid() const;
-		void		set_eval(eval eval_set);
+
+		/**
+		 * @brief Get the zobrist hash of the grid,
+		 * wich is automatically calculated at instanciation
+		 * 
+		 * @return a uint64 containing the current grid's hash
+		 */
 		uint64_t get_hash() const;
-		int	find_blank() const;
-		int	calculate_score();
-		int calculate_linear_colision();
-		void	display_grid() const;
-		void	display_dir() const;
-		void	clear_grid();
+
+
+		/** STATE LOGIC **/
+
+		/**
+		 * @brief Get a vector containing all possible next states of current states
+		 * (children states)
+		 * 
+		 * @return std::vector<State> 
+		 */
 		std::vector<State>	get_possible_moves() const;
-		std::vector<State>		create_path() const;
+
+		/**
+		 * @brief Create a path from head state to current state
+		 * 
+		 * @return std::vector<State> 
+		 */
+		std::vector<State>	create_path() const;
+
+
+		/** UTILS **/
+
+		/**
+		 * @brief prints the underlying grid on std::cout
+		 * 
+		 */
+		void	display_grid() const;
+		/**
+		 * @brief prints the move ( UP, DOWN etc...) used to transition to this state
+		 * 
+		 */
+		void	display_dir() const;
+
+		/**
+		 * @brief free the grid's resources, to use only if you dont need to access the grid anymore.
+		 * eg: state has already been expanded
+		 */
+		void	clear_grid();
+
 	private:
-		//constructor to call when generating children
+		/**
+		 * @brief Constructor that is only called by a parent State constructing its child
+		 * 
+		 * @param parent ptr to parent
+		 * @param index_blank current index of blank tile
+		 * @param index_swap current index of targeted move tile
+		 * @param dir direction in wich the move is happening
+		 */
 		State(const State * parent, int index_blank, int index_swap, direction dir);
-		static size_t total_states;
+
+
+		static size_t total_states;//total number of states ever constructed
 		static int	n; //size of the side of the puzzle
 		static int	size; //number of cells in the puzzle
 
+		/**
+		 * @brief returns the index of the blank tile in the grid
+		 * 
+		 * @return int 
+		 */
+		int	find_blank() const;
+
+		/**
+		 * @brief calculates the score based on a heuristics function, only call this once at instanciation then refer to the "score" variable
+		 * 
+		 * @return int 
+		 */
+		int	calculate_score();
+
+		/**
+		 * @brief a heuristics function based on the linear collision/conflict widely known npuzzle heuristics
+		 * 
+		 * @return int 
+		 */
+		int calculate_linear_colision();
 		/* HASH related stuff */
+
+		/**
+		 * @brief a double array containing all randomly generated hash used in the zobrist hash
+		 * 
+		 */
 		static std::vector<std::vector<uint64_t>> hash_grid;
 
+		/**
+		 * @brief a transposition table containing all previously calculated scores
+		 * 
+		 */
 		static std::unordered_map<uint64_t, int> transposition_table;
 
 		uint64_t	hash;
 
-
+		/**
+		 * @brief calculate a new zobrist hash from a move
+		 * 
+		 * @param index_blank 
+		 * @param index_swap 
+		 */
 		void			calculate_hash(int index_blank, int index_swap);
+
+		/**
+		 * @brief calculate initial zobrist hash, call only on initial state, then on every expansion call only calculate_hash
+		 * 
+		 */
 		void			calculate_start_hash();
+
+		/**
+		 * @brief call once to generate the random positions hashes
+		 */
 		void	generate_hash_grid();
 };
